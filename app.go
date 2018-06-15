9108215a3591795daff22bb904d6f3ab58d42f41
chida <chida@fringe81.com>
2018-06-15 14:13:57 +0900

    green

--- app.go
+++ app.go
@@ -19,5 +19,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(os.Stdout, "STDOUT : " + d + "\n") // stdout
 	fmt.Fprint(os.Stderr, "STDERR : " + d + "\n") // stderr
 
-	fmt.Fprintf(w, "[blue] Path : %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "[green] Path : %s", r.URL.Path[1:])
 }
